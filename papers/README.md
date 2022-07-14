592fc9efa72dc6311099869d5c03fe1190a94252
Matthew Kenworthy <kenworthy@strw.leidenuniv.nl>
2022-07-14 15:54:25 -0700

    scripted index.html generation

--- papers/README.md
+++ papers/README.md
@@ -10,3 +10,11 @@ Pulling the PDFs from belterwijde:
 Pushing papers to belterwijde:
 
     rsync -rva *.pdf kenworthy@belterwijde.strw.leidenuniv.nl:public_html/papers/.
+
+
+Making the page using `python make_page.py`
+
+awk -F '\t' '{ print "cp " $1 " \047" $4".jpg\047" }' < allimgs.txt > jpgs.sh
+
+awk -F '\t' '{ print "echo \"" $2"\\n" $3 "\" >\047" $4.".txt\047" }' < allimgs.txt > run.sh
+
--- papers/make_page.py
+++ papers/make_page.py
@@ -1,32 +1,65 @@
-
 import os
 import math
 import requests
 import argparse
 import csv
 
+import sys
 
-#url = "https://home.strw.leidenuniv.nl/~kenworthy/papers/2021ExA...tmp..124S.pdf"
-#title = "Detecting life"
-#author = "Snellen, I. A. G., Snik, F., Kenworthy, M., et al."
-#year = "2021"
-#jourref = "ExA...tmp"
-#adsurl = "https://ui.adsabs.harvard.edu/abs/2021ExA...tmp..124S"
+'''make_page.py = parses a list of bibcodes from a file and then produces an HTML page
+with the links to the papers, and if a bibcode.txt and bibcode.jpg|png file exists, add a side panel figure'''
 
+# TODO shorten the names of the journals
 
-#sample = """
-#    <p><a href="{0}">{1}</a><br /> {2} ({3})<br /> {4}, <a href="{5}">[ADS]</a></p>
-#        """.format(url, title, author, year, jourref, adsurl)
+html_out = 'index.html'
 
-# Using triple quotes will give you unwanted indentation. To align your indentation,
-# you can use textwrap module from pythons stdlib
-import textwrap
-#sample = textwrap.dedent(sample)
+filenames = ['head.txt','tmp.html','tail.txt']
 
-#print(sample)
+##https://stackoverflow.com/questions/384076/how-can-i-color-python-logging-output
+import logging
+class CustomFormatter(logging.Formatter):
 
-#message = f'<p><a href="{url}">{title}</a><br /> {author} ({year})<br /> {jourref}, <a href="{adsurl}">[ADS]</a></p>'
-#print(message)
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+# create logger with 'spam_application'
+logger = logging.getLogger("MAKE_PAGE")
+logger.setLevel(logging.DEBUG)
+
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+ch.setFormatter(CustomFormatter())
+
+logger.addHandler(ch)
+
+#logger.debug("debug message")
+#logger.info("info message")
+#logger.warning("warning message")
+#logger.error("error message")
+#logger.critical("critical message")
+
+# write the htmlized paper list to this temporary file
+outfile = open('tmp.html','w')
+
 
 # https://stackoverflow.com/questions/4529815/saving-an-object-data-persistence
 def pickle_loader(filename):
@@ -43,6 +76,14 @@ def save_object(obj, filename):
         pickle.dump(obj, outp, pickle.HIGHEST_PROTOCOL)
 
 
+def authorlist(authors, nauthors=3):
+    '''authorlist - pretty print list of authors for HTML'''
+    st = ""
+    if len(authors) < nauthors:
+        return ", ".join(authors)
+
+    return ", ".join(authors[:nauthors])+" et al."
+
 
 def get_bibinfo(bibcode):
 
@@ -52,7 +93,7 @@ def get_bibinfo(bibcode):
     # see if file exists
     if os.path.isfile(fin):
         # read in from the pickle
-        print(f'pickle {fin} found, reading in...')
+ #       print(f'pickle {fin} found, reading in...')
         res=pickle_loader(fin)
     else:
         # if no, download and save pickle
@@ -96,22 +137,62 @@ if __name__ == '__main__':
 
     bibcodes = data[1].split("\t")
 
-    for bib in bibcodes[10:14]:
-        print(bib)
+    for bib in bibcodes:
         ob = get_bibinfo(bib)
 
         url = f'https://home.strw.leidenuniv.nl/~kenworthy/papers/{ob.bibcode}.pdf'
-
+#     journal = journal + f', {ob.page[0]} '
+# TypeError: 'NoneType' object is not subscriptable
         journal = ""
         if hasattr(ob, 'pub'):
-            journal = journal + f'{ob.pub} '
+            journal = journal + f'{ob.pub}'
         if hasattr(ob, 'volume'):
-            journal = journal + f'{ob.volume} '
+            journal = journal + f', {ob.volume}'
         if hasattr(ob, 'page'):
-            journal = journal + f'{ob.page}, '
+            try:
+                journal = journal + f', {ob.page[0]} '
+            except:
+                logger.warning(f'no page number for {bib}. Skipping page number insertion.')
 
         jourref = journal
-        print(ob.year)
 
-        message = f'<p><a href="{url}">{ob.title[0]}</a><br /> {ob.author} ({ob.year})<br /> {journal}<a href="https://ui.adsabs.harvard.edu/abs/{ob.bibcode}">[ADS]</a></p>'
-        print(message)
+        # if there's a file called bibcode.txt then read it in
+        # check that bibcode.jpg exists
+        # build up the html snippet
+        if  os.path.isfile(bib+'.txt'):
+
+            pin = 'not found'
+            if os.path.isfile(bib+'.png'):
+                pin = bib+'.png'
+            if os.path.isfile(bib+'.jpg'):
+                pin = bib+'.jpg'
+
+            logger.info(f'Found {bib}.txt and found {pin} image\n\n')
+
+
+            # read in text file to get maintxt and alttext
+            fi = open(bib+'.txt',newline='\n')
+            (xin) = fi.read().split('\n')
+            alttxt, maintxt = xin[0], xin[1]
+
+            fauth = ob.author[0].split(',')[0]
+
+            urlbackref = f'<a href="https://home.strw.leidenuniv.nl/~kenworthy/papers/{bib}.pdf">{fauth} et al. ({ob.year})</a>'
+
+            sidepanel = f'<p><label for="{bib}" class="margin-toggle">&#8853;</label><input type="checkbox" id="{bib}" class="margin-toggle"/><span class="marginnote"><img src="{pin}" alt="{alttxt}"/>{maintxt} {urlbackref}.</span></p>'
+
+            outfile.write((sidepanel+"\n"))
+
+        message = f'<p><a href="{url}">{ob.title[0]}</a><br /> {authorlist(ob.author)} ({ob.year})<br /> {journal} <a href="https://ui.adsabs.harvard.edu/abs/{ob.bibcode}">[ADS]</a></p>'
+        outfile.write(message+"\n\n")
+
+
+# put them all together
+
+
+# write out the html page using filenames defined at the top!
+with open(html_out, 'w') as outfile:
+    for fname in filenames:
+        with open(fname) as infile:
+            outfile.write(infile.read())
+
