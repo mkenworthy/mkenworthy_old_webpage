b4dec7e4ee561bd45fdd22f4c12a17dc82e71000
Matthew Kenworthy <kenworthy@strw.leidenuniv.nl>
2022-08-22 13:26:27 +0200

    removed old API key

--- papers/make_list.py
+++ papers/make_list.py
@@ -1,17 +1,2 @@
 
 
-curl -H 'Authorization: Bearer <token>' 'https://api.adsabs.harvard.edu/v1/search/query?q=star'
-
-curl -H 'Authorization: Bearer HZeLrdZnKSQxw1xHbiPYM9QgYNl2WL3ZcnVV2GtC' 'https://api.adsabs.harvard.edu/v1/search/query?q=star'
-
-HZeLrdZnKSQxw1xHbiPYM9QgYNl2WL3ZcnVV2GtC
-
-
-qGMnfKiwT_ydy8eoGu7esw
-
-curl -H "Authorization: Bearer HZeLrdZnKSQxw1xHbiPYM9QgYNl2WL3ZcnVV2GtC" https://api.adsabs.harvard.edu/v1/biblib/libraries/qGMnfKiwT_ydy8eoGu7esw | python -m json.tool
-
-
-r = requests.get("https://api.adsabs.harvard.edu/v1/biblib/libraries/qGMnfKiwT_ydy8eoGu7esw", \
-                 headers={"Authorization": "Bearer HZeLrdZnKSQxw1xHbiPYM9QgYNl2WL3ZcnVV2GtC"})
-print(r.json())
